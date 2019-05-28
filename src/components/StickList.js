a941e21da9872fe19711e94388b280e4892a8367
Reed MacDonald <reedpmacdonald@gmail.com>
2019-05-27 23:14:56 -0700

    finish post route

--- src/components/StickList.js
+++ src/components/StickList.js
@@ -13,6 +13,8 @@ class Chopsticks extends Component{
         super(props);
         this.state = {
             listOfSticks: [{length:'idk'}],
+            moduleShowing: false,
+            idToSend:null
         };
       }
 
@@ -53,13 +55,93 @@ class Chopsticks extends Component{
               <div style={{'backgroundColor':'yellow','height':'50%','width':'50%'}}> 
                   <h2>Your chopsticks</h2>
                   {console.log(this.props)}
-                  <h4>{this.props.newList==undefined?undefined:this.props.newList.map((element,index)=>
+                  {this.props.newList==undefined?undefined:this.props.newList.map((element,index)=>
+                  
                   <h2>
                   {element.length+"   "} 
                   {element.width+"   "} 
                   {element.color+"   "}
+                  
                   <button type="submit" onClick={()=>{this.props.changeSticks(element)}}>Eat with these</button>
-                  </h2>)}</h4>
+                  <button
+                  onClick={async () => {
+                    try {
+                      const response = await fetch(`http://localhost:8000/api/v1/chopsticks/${element.id}`, {
+                        method: 'DELETE',
+                        credentials: 'include',
+                        headers: {
+                          'Content-Type': 'application/json'
+                        }
+                      });
+                
+                
+                      if(response.ok){
+                        const responseParsed = await response.json();
+                        console.log(responseParsed)
+                      }
+                      this.getChopsticks()
+                    } catch(err){
+                      console.log(err)
+                    }
+                
+                  }}
+                  >Delete</button>
+                  <button
+                  onClick={
+                      ()=>{
+                          
+                              this.setState({
+                                  moduleShowing:true,
+                                  idToSend:element.id
+                              })
+                          
+                      }
+                  }>Update</button>
+                  </h2>)}
+                  {this.state.moduleShowing?
+                  <form>
+                <input id = 'uniqueIDFive' placeholder='length'/>
+                <input id = 'uniqueIDSix' placeholder='width'/>
+                <input id = 'uniqueIDSeven' placeholder='color'/>
+                <input id = 'uniqueIDEight' placeholder='message'/>
+                <button onClick={
+                    async ()=>{
+                        var length = document.getElementById("uniqueIDFive").value
+                        var width = document.getElementById("uniqueIDSix").value
+                        var color = document.getElementById("uniqueIDSeven").value
+                        var message = document.getElementById("uniqueIDEight").value
+                            let objectToSendOne = {
+                                length:length.toString()+'px',
+                                width:width.toString()+'px',
+                                color:color.toString(),
+                                message:message,
+                                owner:this.props.owner,
+                                created_by_id:'Reed'
+                            }
+                            try {
+
+                                const registerCall = await fetch(`http://localhost:8000/api/v1/chopsticks/${this.state.idToSend}`, {
+                                  method: 'PUT',
+                                  body: JSON.stringify(objectToSendOne),
+                                  credentials: 'include',
+                                  headers: {
+                                    'Content-Type': 'application/json'
+                                  }
+                          
+                                })
+                          
+                                const response = await registerCall.json()
+                                console.log(response, 'from the flask server on localhost:8000')
+                                this.getChopsticks()
+                          
+                              } catch(err){
+                                console.log(err)
+                                this.getChopsticks()
+                              }
+                    }
+                } type="button" > Update Chopsticks</button>
+                </form>:undefined}
+                  
 
             </div>
 
